b08a1e3d7a34b456c4e4327a6356d882553837e2
upanesh <upanesh@gmail.com>
2022-10-12 21:25:18 +0000

    Added Article list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,7 @@ class ArticlesController < ApplicationController
     def show
        @article = Article.find(params[:id]) 
     end
+    def index
+        @all_articles = Article.all
+    end
 end
