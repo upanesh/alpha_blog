3f49314fa29a686c813501f09c8b6d13bebb84e6
upanesh <upanesh@gmail.com>
2022-10-18 21:55:32 +0000

    Final App

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
     
-    
     def show
     end
     
@@ -38,7 +37,6 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        
         @article.destroy
         redirect_to articles_path        
     end
