5943ebc32d375cd77416612a082137c58ad1c9d8
upanesh <upanesh@gmail.com>
2022-10-21 17:39:51 +0000

    add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
 
+    
     def new
         @user = User.new
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)    
+            flash[:notice] = "#{@user.username} was update successfully"
+            redirect_to articles_path
+        else
+            render 'edit'    
+        end
     end
     
     def create
